867b663ce9087fc837993be1e6ad2c4f418decd3
MatiasMartina <matiasmartina97@gmailcom>
2023-12-07 19:25:59 -0300

    aversianda

---
+++ main.py
@@ -0,0 +1,70 @@
+from models.player.main_player import Jugador
+import pygame as pg
+from pygame.locals import * 
+from models.constantes import * 
+from world import *
+pg.init()
+
+
+screen_height = 800
+screen_width = 800
+
+clock = pg.time.Clock()
+screen = pg.display.set_mode((screen_height,screen_width))
+pg.display.set_caption("BA-ME-APRO")
+
+# CARGAMOS BACKGROUNG
+
+img_background = pg.image.load('assets\img\\background\\background1.png')
+scaled_background = pg.transform.scale(img_background, (1200, 800))
+
+#INSTANCIAMOS
+player = Jugador(0 , 0, frame_rate = 1, speed_walk = 20, speed_run = 30, delta_ms = 1, speed_jump= 50  )
+
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1], 
+[1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1], 
+[1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+world = World(world_data)
+
+running = True
+
+while running:
+    pressed_key = pg.key.get_pressed()
+    event_list = pg.event.get()
+    for event in event_list:
+        if event.type == QUIT:
+            running = False
+    
+    # BLITEAMOS
+    # UPDATEAMOS
+    pg.display.update()  
+         # SCREEN
+    screen.blit(scaled_background, scaled_background.get_rect())
+    delta_ms = clock.tick(FPS)
+    #WORLD
+    world.draw_grid(screen)
+    world.draw(screen)
+    #PLAYER
+    player.update(pressed_key,delta_ms,screen, world)
+pg.quit()
+
